e20500de33db6fce47b4ff683eaf46648837437e
Memo56-FB <memo56_fb@hotmail.com>
2026-03-26 21:30:03 -0600

    feat: add starfield animated background

---
+++ app/components/layout/Background/Background.tsx
@@ -0,0 +1,9 @@
+import styles from './Background.module.css'
+
+export const Background = () => {
+  return (
+    <div className={styles['stars-wrapper']}>
+        <div className={styles.stars}></div>
+    </div>
+  )
+}
