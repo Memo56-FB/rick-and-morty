5beea80dcb69e457bc299d9982f659e8ea1b2e1d
Memo56-FB <memo56_fb@hotmail.com>
2026-03-28 07:19:43 -0600

    refactor: move home page styles into css module

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,42 @@
-import Image from 'next/image';
+import Image from 'next/image'
 import image from '../public/Rick_and_Morty.svg'
 import { DesktopCharacterShowcaseContainer } from '@/app/components/cards/DesktopCharacterShowcase/DesktopCharacterShowcaseContainer'
 import { MobileCharacterCarouselContainer } from '@/app/components/cards/MobileCharacterCarousel/MobileCharacterCarouselContainer'
 import { FavoritesPanelContainer } from '@/app/components/favorites/FavoritesPanel/FavoritesPanelContainer'
 import { getCharactersPage } from '@/lib/rick-and-morty/rick-and-morty.service'
+import styles from './page.module.css'
 
 export default async function Home() {
   const initialCharactersPage = await getCharactersPage()
 
   return (
-    <section className='flex min-h-full w-full flex-col items-center px-4 md:px-8'>
-      <div className='mt-8 flex w-full justify-center md:mt-10 md:max-w-6xl md:justify-end md:pr-12'>
+    <section className={styles.page}>
+      <div className={styles.logoWrapper}>
         <Image
           src={image}
           alt='rick y morty'
           width={332}
           height={101}
           loading='eager'
-          className='h-auto'
+          className={styles.logo}
         />
       </div>
 
-      <div className='md:hidden'>
+      <div className={styles.mobileContent}>
         <MobileCharacterCarouselContainer
           initialCharactersPage={initialCharactersPage}
         />
       </div>
 
-      <div className='hidden w-full flex-1 items-center justify-center py-8 md:relative md:z-20 md:-mb-24 md:flex md:py-12'>
+      <div className={styles.desktopContent}>
         <DesktopCharacterShowcaseContainer
           initialCharactersPage={initialCharactersPage}
         />
       </div>
 
-      <div className='mt-auto w-full grid place-items-center pt-8 self-end md:hidden'>
+      <div className={styles.favorites}>
         <FavoritesPanelContainer />
       </div>
     </section>
-  );
+  )
 }
